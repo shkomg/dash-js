d40f00fc8e87010ca447087986ed64e91d0bae5b
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-10-22 14:38:42 +0300

    git url added

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'shkomg:dash-js',
     version: '0.0.1',
     summary: "A smart package that makes it easy to use Dash-js",
-    git: '',
+    git: 'https://github.com/shkomg/dash-js',
     documentation: 'README.md'
 });
 
